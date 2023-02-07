fdd3beb47b5ab547473eb14d2cdbd78cb432f9ee
Nelstrooy <devilord992@gmail.com>
2023-02-07 16:16:06 +0800

    first commit

---
+++ Hello.py
@@ -0,0 +1,13 @@
+#Nelstrooy Galat Anak Parti, 20DDT21F1045,(0c*9/5)+32
+
+name=input("Enter name : ")
+studID=input("Enter student id: ")
+celcius=float(input("Enter current celcius: "))
+
+farenheit=(celcius*9/5)+32
+
+print("Your current temperature in Farenheit is : ",farenheit)
+
+    
+
+
